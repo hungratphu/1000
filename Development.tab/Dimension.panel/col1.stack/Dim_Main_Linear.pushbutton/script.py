2dc87affa3b73aaf6dcaa2d07ef8a0e4d9dd7a61
hungratphu <80562943+hungratphu@users.noreply.github.com>
2024-03-07 16:37:28 -0500

    k

--- Development.tab/Dimension.panel/col1.stack/Dim_Main_Linear.pushbutton/script.py
+++ Development.tab/Dimension.panel/col1.stack/Dim_Main_Linear.pushbutton/script.py
@@ -13,6 +13,7 @@ Author: Hung Nguyen """
 # ---------------------------------------------------------
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import *
+from Autodesk.Revit.UI.Selection import *
 
 # pyRevit Imports
 from pyrevit import forms
@@ -24,6 +25,7 @@ from System.Collections.Generic import List
 
 # Custom Imports
 from Snippets._transaction import *
+import time
 
 # ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
 # ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
@@ -39,8 +41,11 @@ app             = __revit__.Application                 # type: UIApplication.Ap
 # ║║║  ╠═╣  ║  ║║║
 # ╩ ╩  ╩ ╩  ╩  ╝╚╝  MAIN
 # ---------------------------------------------------------
+start_time = time.time()
+
 all_dimension_types = FilteredElementCollector(doc).OfClass(DimensionType).ToElements()
 all_dimension       = FilteredElementCollector(doc).OfClass(Dimension).ToElements()
+all_constraints     = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Constraints).WhereElementIsNotElementType().ToElements()
 
 all_linear_dimension_type = [dim for dim in all_dimension_types if dim.StyleType == DimensionStyleType.Linear]
 all_linear_dimension      = [dim for dim in all_dimension if dim.DimensionShape == DimensionShape.Linear]
@@ -52,24 +57,77 @@ main_linear_dimension = forms.SelectFromList.show(all_linear_dimension_type,
                                                    button_name="Select the main dimension",
                                                    multiselect=False,
                                                    name_attr="Name")
-if main_linear_dimension:
-    print("You select main dimension as: ", main_linear_dimension.Name)
+
+
+other_than_main_linear_dimension_and_constraint = [dim for dim in all_linear_dimension
+                                                   if dim.DimensionType != main_linear_dimension
+                                                   and dim.Category.BuiltInCategory != BuiltInCategory.OST_Constraints]
+
+print("Find: {} constraints".format(len(all_constraints)))
+print("Find: {} dimension to action".format(len(other_than_main_linear_dimension_and_constraint)))
+
 
 # ╔╦╗╦═╗╔═╗╔╗╔╔═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔
 #  ║ ╠╦╝╠═╣║║║╚═╗╠═╣║   ║ ║║ ║║║║
 #  ╩ ╩╚═╩ ╩╝╚╝╚═╝╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝ TRANSACTION
 # --------------------------------------------------------
-with transaction(doc, "1000: Change to main dimension type", debug=True):
-    if main_linear_dimension:
-        for dimension in all_linear_dimension:
-            if dimension.AreReferencesAvailable:
-                # print(dimension.Id, "\t", dimension.Name)
-                try:
-                    dimension.DimensionType = main_linear_dimension
-                except:
-                    pass
+# for dimension in other_than_main_linear_dimension:  # type: Dimension
+#     if dimension.IsValidObject:
+#         if not dimension.AreReferencesAvailable and dimension.GroupId != ElementId(-1):
+#             print("002: Id, GroupId, Name:", dimension.Id, dimension.GroupId)
+#             if dimension.View:
+#                 print(dimension.View.Name)
+#
+# print("|"*50)
 
-forms.alert("You are now able to purge unused dimension"
-            "All dimension are change to main dimension")
+with transaction_group(doc, "1000: Change to main dimension type", debug=True):
+
+
+    if main_linear_dimension:
+
+        selected_id_list = []
+
+        for dimension in other_than_main_linear_dimension_and_constraint:   # type: Dimension
+            if dimension.IsValidObject:
+                if dimension.GroupId == ElementId(-1):
+                    with transaction(doc, "Change model dimension"):
+
+                        if dimension.AreReferencesAvailable:
+                            try:
+                                dimension.DimensionType = main_linear_dimension
+                                # family.Set(ElementId(main_linear_dimension.Id))
+                            except:
+                                print("Detect_Model: ID: {}, at: {}, cat: {}, name:{}".format(dimension.Id,
+                                                                                              dimension.View,
+                                                                                              dimension.Category.Name,
+                                                                                              dimension.Name))
+                        elif not dimension.AreReferencesAvailable:
+                            try:
+                                dimension.DimensionType = main_linear_dimension
+                                # family.Set(ElementId(main_linear_dimension.Id))
+                            except:
+                                print("Detect_ViewSpecific: ID: {}, at: {}, cat: {}, name:{}".format(dimension.Id,
+                                                                                                     dimension.View,
+                                                                                                     dimension.Category.Name,
+                                                                                                     dimension.Name))
+
+                else:
+                    if dimension.Id not in selected_id_list:
+                        selected_id_list.append(dimension.Id)
+
+            with transaction(doc, "change", debug=True):
+                for con in all_constraints:
+                    selected_id_list.append(con)
+
+                new_selection = List[ElementId](selected_id_list)
+                selection = uidoc.Selection.SetElementIds(new_selection)
+
+        # forms.alert("NEXT STEP:"
+        #             "Purge unused dimension")
 
 # ---------------------------------------------------------
+
+end_time = time.time()
+count_time =  end_time - start_time
+# print("EXECUTION TIME: "
+#       "{} seconds".format(count_time))
