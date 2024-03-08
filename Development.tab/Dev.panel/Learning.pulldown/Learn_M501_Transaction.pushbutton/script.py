fe30b1c880fa455de1b9e84f0b9ae6e2ac4244c1
hungratphu <80562943+hungratphu@users.noreply.github.com>
2024-03-07 20:32:39 -0500

    k

--- Development.tab/Dev.panel/Learning.pulldown/Learn_M501_Transaction.pushbutton/script.py
+++ Development.tab/Dev.panel/Learning.pulldown/Learn_M501_Transaction.pushbutton/script.py
@@ -54,38 +54,6 @@ PATH_SCRIPT     = os.path.dirname(__file__)
 #  ╩ ╩╚═╩ ╩╝╚╝╚═╝╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝ TRANSACTION
 # ---------------------------------------------------------
 
-import contextlib, traceback
-
-@contextlib.contextmanager
-def transaction(doc, title, debug=False):
-    """Context Manager that will initiate Transaction .Start() and .Commit() methods.
-    In case of an error - it will .RollBack() transaction and display error message if debug = True
-    @param doc:     Document where Transaction should be created
-    @param title:   Name of Transaction
-    @param debug:   True - Display error messages / False - No error messages
-    """
-
-    t = Transaction(doc, title)
-    t.Start()
-    try:
-        yield
-        t.Commit()
-    except:
-        t.RollBack()
-        if debug:
-            print(traceback.format_exc())
-
-# ---------------------------------------------------------
-
-@ contextlib.contextmanager
-def try_except(debug=False):
-    try:
-        yield
-    except:
-        if debug:
-            print(traceback.format_exc())
-# ---------------------------------------------------------
-
 
 # ╦═╗╔═╗╔═╗╔╦╗  ╔╦╗╔═╗╦═╗╔═╗
 # ╠╦╝║╣ ╠═╣ ║║  ║║║║ ║╠╦╝║╣
--- Development.tab/Grids.panel/extent1.stack/Extent Type_Set 2D to 3D Maximum_ActiveView.pushbutton/script.py
+++ Development.tab/Grids.panel/extent1.stack/Extent Type_Set 2D to 3D Maximum_ActiveView.pushbutton/script.py
@@ -45,20 +45,21 @@ active_level = active_view.GenLevel
 all_grids = (FilteredElementCollector(doc, active_view.Id).OfCategory(BuiltInCategory.OST_Grids).
              WhereElementIsNotElementType().ToElements())
 
+visible_grids = [grid for grid in all_grids if grid.CanBeVisibleInView()]
+
 with transaction(doc, "1000: Set Extent 2D/3D", True):
 
-    for grid in all_grids:
+    for grid in visible_grids:
         grid = grid  # type: Grid
 
         # Set to 3D Extent Type
-        if grid.CanBeVisibleInView(active_view):
-            grid.SetDatumExtentType(DatumEnds.End0, active_view, DatumExtentType.Model)
-            grid.SetDatumExtentType(DatumEnds.End1, active_view, DatumExtentType.Model)
+        grid.SetDatumExtentType(DatumEnds.End0, active_view, DatumExtentType.Model)
+        grid.SetDatumExtentType(DatumEnds.End1, active_view, DatumExtentType.Model)
 
 
         # Set to 2D Extent right after to the 3D extent of grid
-            grid.SetDatumExtentType(DatumEnds.End0, active_view, DatumExtentType.ViewSpecific)
-            grid.SetDatumExtentType(DatumEnds.End1, active_view, DatumExtentType.ViewSpecific)
+        grid.SetDatumExtentType(DatumEnds.End0, active_view, DatumExtentType.ViewSpecific)
+        grid.SetDatumExtentType(DatumEnds.End1, active_view, DatumExtentType.ViewSpecific)
 
 # ---------------------------------------------------------
 print('-' * 50)
